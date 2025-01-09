d59737f847c0b90aa7e50c17e8e8a0167fd0136a
bharati-swami <bharati.nishu@gmail.com>
2025-01-09 13:39:45 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    return {
+        skills: ['JavaScript', 'React', 'Node', 'HTML', 'CSS', 'Python'],
+        getSkills: function() {
+            return this.skills;
+        }
+    }
+}
